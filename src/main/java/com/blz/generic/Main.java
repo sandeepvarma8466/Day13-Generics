538b0f11bbd4bbc6a44b99ef9b4b5ce147c62322
sandeepvarma8466 <svarma852@gmail.com>
2022-07-06 21:18:18 +0530

    Test Maximum

---
+++ src/main/java/com/blz/generic/Main.java
@@ -0,0 +1,9 @@
+package com.blz.generic;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Welcome to check max of three variables");
+    }
+}
