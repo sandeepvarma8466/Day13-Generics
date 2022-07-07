24ec3b89406a8b2ecf99f3f5b1419827e7afcd88
sandeepvarma8466 <svarma852@gmail.com>
2022-07-07 08:04:42 +0530

    given 3 strings find maximum

--- src/main/java/com/blz/generic/TestMaximum.java
+++ src/main/java/com/blz/generic/TestMaximum.java
@@ -1,17 +1,17 @@
 package com.blz.generic;
 
 public class TestMaximum {
-    float x, y, z;
+    String x, y, z;
 
-    public TestMaximum(float x, float y, float z) {
+    public TestMaximum(String x, String y, String z) {
         this.x = x;
         this.y = y;
         this.z = z;
     }
 
-    // testMaximum method created for checking max of three integers
-    public static Float testMaximum(Float x, Float y, Float z) {
-        Float max = x;
+    // testMaximum method created for checking max of three Strings
+    public static String testMaximum(String x, String y, String z) {
+        String max = x;
         if (y.compareTo(max) > 0)
             max = y;
         if (z.compareTo(max) > 0)
@@ -20,8 +20,8 @@ public class TestMaximum {
         return max;
     }
 
-    // printMax method created for displaying maximum values from the given 3 variables.
-    public static void printMax(Float x, Float y, Float z, Float max) {
+    // printMax method created for displaying maximum values from the given 3 Strings.
+    public static void printMax(String x, String y, String z, String max) {
         System.out.printf("The max of %s, %s and %s is %s\n", x, y, z, max);
 
     }
--- src/test/java/com/blz/maximum/MaximumTest.java
+++ src/test/java/com/blz/maximum/MaximumTest.java
@@ -5,22 +5,22 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class MaximumTest {
-    //TC 1.1 Given Max Number at 1st position return the Same Number.
+    //TC 1.1 Given Max String at 1st position return the Same String.
     @Test
     public void floatMax1() {
-        float actualValue = TestMaximum.testMaximum(8.2f, 6.5f, 4.5f);
+        String actualValue = TestMaximum.testMaximum("dogs", "cat", "dog");
         Assert.assertTrue(true);
     }
-   // TC 1.2 Given Max Number at 2nd position return the Same Number.
+   // TC 1.2 Given Max String at 2nd position return the Same String.
     @Test
     public void floatMax2() {
-        float actualValue = TestMaximum.testMaximum(3.8f,12.8f,6.8f);
+        String actualValue = TestMaximum.testMaximum("cat","goat","dog");
         Assert.assertTrue(true);
     }
-   //TC 1.3 Given Max Number at 3rd position return the Same Number.
+   //TC 1.3 Given Max String at 3rd position return the Same String.
     @Test
     public void floatMax3() {
-        float actualValue = TestMaximum.testMaximum(5.2f, 6.3f,14.8f);
+        String actualValue = TestMaximum.testMaximum("bat", "ball","wicket");
         Assert.assertTrue(true);
     }
 }
